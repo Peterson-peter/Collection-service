c5895d0da74a9f20ea5242bc1fb7b669c47041d4
Pete <pepeter@microsoft.com>
2019-04-16 13:32:18 -0700

    fixed and now can get started

--- src/Program.cs
+++ src/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace collection
+namespace Collection_service
 {
     public class Program
     {
